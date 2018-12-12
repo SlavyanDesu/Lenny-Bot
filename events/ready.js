85ea40caa51af7814019353bbea472b7890fcd87
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2018-12-12 08:57:02 +0700

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,18 @@
+module.exports = (client) => {
+  const activities = [
+    'with Slavyan',
+    'osu!',
+    'with depression',
+    'with members',
+    'ball',
+    'Nekopara',
+    'PLAYERUNKNOWN\'S BATTLEGROUNDS'
+    ];
+  
+  console.log(`${client.user.username} is online!!`);
+  
+  setInterval(() => {
+    const index = Math.floor(Math.random() * Math.floor(activities.length - 1) + 1);
+    client.user.setActivity(activities[index]);
+  }, 80000);
+};
