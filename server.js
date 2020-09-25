b14214a36cf18962ef1bd292a151f2e19c07b7c7
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2020-09-25 07:59:48 +0700

    Fix typo

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ fs.readdir('./commands/', (err, files) => {
   files.forEach(f => {
     if (!f.endsWith('.js')) return;
     let props = require(`./commands/${f}`);
-    console.log(`Loadinh ${props.help.name} command... done!`);
+    console.log(`Loading ${props.help.name} command... done!`);
     client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
